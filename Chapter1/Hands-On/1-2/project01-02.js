e351c3e3f44c836704a3cc91093e3a7b1244770f
nmanclank <7421094+nmanclank@users.noreply.github.com>
2024-09-12 21:20:31 -0700

    Completed Hands on 1-4

--- Chapter1/Hands-On/1-2/project01-02.js
+++ Chapter1/Hands-On/1-2/project01-02.js
@@ -10,4 +10,4 @@
 
 //define variables for service name and service speed
 let service1Name = "Basic",service2Name = "Express",service3Name = "Extreme",service4Name = "Ultimate",
-service1Speed = "0 Mbps",service2Speed = "100 Mbps",service3Speed = "500 Mbps",service4Speed = "1 Gig"
+service1Speed = "0 Mbps",service2Speed = "100 Mbps",service3Speed = "500 Mbps",service4Speed = "1 Gig";
