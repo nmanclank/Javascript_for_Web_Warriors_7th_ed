e76f2f53587032961ca2b4c347644ac2a752396d
nmanclank <7421094+nmanclank@users.noreply.github.com>
2024-12-23 12:54:14 -0700

    Ch9 Complete

--- Chapter9/Hands-On/9-4/project09-04.js
+++ Chapter9/Hands-On/9-4/project09-04.js
@@ -19,6 +19,33 @@ window.addEventListener("puzzleSolved", updateRecord);
 
 // Event listener that is run when the page loads
 window.addEventListener("load", function() {
-
+      if (document.cookie) {
+            bestText.textContent = getBestTime() + " seconds"
+      }
 });
 
+function getBestTime() {
+      if (document.cookie) {
+
+      let cookieArray = document.cookie;
+      cookieArray = cookieArray.split("=");
+      return parseInt(cookieArray[1]);
+      }
+      else {
+            return 9999;
+      }
+}
+
+function updateRecord() {
+      let solutionTime = parseInt(document.getElementById("timer").value);
+      let bestTime = getBestTime();
+
+      if (solutionTime < bestTime) {
+            bestTime = solutionTime
+            bestText.textContent = bestTime + " seconds";
+
+
+      }
+      let maxAge = 7776000;
+      document.cookie = "puzzle8Best" +  bestTime + "maxage =" + 60*60*24*90;
+}
